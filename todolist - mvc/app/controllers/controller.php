7508578433666ad74286de9bda09004f087b6683
MATEUS DA SILVA MARINHO <20231134010012@ifrn.local>
2026-09-18 11:47:46 -0300

    atualizando os arquivos' exit

---
+++ todolist - mvc/app/controllers/controller.php
@@ -0,0 +1,34 @@
+<?php 
+
+require_once __DIR__ . '/../models/tarefa.php';
+
+class TarefaController{
+    private $tarefaModel; 
+
+    public function __construct(){
+        $this->tarefaModel = new Tarefa(); 
+    }
+
+    public function index(){
+        $tarefas = $this->tarefaModel->listar(); 
+        include __DIR__ . '/../views/listar.php'; 
+    }
+
+    public function criar(){
+        if(isset($_POST['descricao']) && !empty(trim($_POST['descricao']))){
+            $this->tarefaModel->criar($_POST['descricao']);
+        }
+        header("Location: index.php");
+    }
+
+    public function excluir(){
+        if(isset($_GET['id'])){
+            $this->tarefaModel->excluir($_GET['id']);
+        }
+        header("Location: index.php"); 
+    }
+    
+}
+
+
+?>
---
+++ todolist - mvc/index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . '/app/controllers/controller.php';
+
+$controller = new TarefaController();
+
+$action = $_GET['action'] ?? 'index';
+
+switch ($action) {
+    case 'criar':
+        $controller->criar();
+        break;
+    
+    case 'excluir' :
+        $controller->excluir();
+        break;
+
+    default :
+        $controller->index();
+}
+?>
